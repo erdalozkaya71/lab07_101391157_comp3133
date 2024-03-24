cf9aecddd2fcaee53bda35c88ba776073e8ce291
erdalozkaya71 <erdalozkaya71@hotmail.com>
2024-03-24 17:18:01 -0400

    second commit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,13 +2,14 @@ import { Component, Input } from '@angular/core';
 import { NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
+import { RemoveSpacesPipe } from '../remove-spaces.pipe';
 
 @Component({
   standalone: true,
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css'],
-  imports: [FormsModule, NgIf, UpperCasePipe],
+  imports: [FormsModule, NgIf, UpperCasePipe, RemoveSpacesPipe],
 })
 export class HeroDetailComponent {
   @Input() hero?: Hero;
